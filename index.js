d8e3056169883f5987e843665e8c08d62135b792
royalngr2216 <royalngr2216@gmail.com>
2026-06-05 18:35:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
@@ -19,13 +18,29 @@ const TOKEN = process.env.TOKEN;
 // SOURCE CHANNEL : DESTINATION CHANNEL
 const CHANNELS = {
 
+    // TH11
+    "SOURCE_TH11_ID": "DEST_TH11_ID",
+
+    // TH12
+    "SOURCE_TH12_ID": "DEST_TH12_ID",
+
+    // TH13
+    "SOURCE_TH13_ID": "DEST_TH13_ID",
+
     // TH14
-    "1478369331376160928":
-    "1512350167209082981",
+    "1478369331376160928": "1512350167209082981",
 
     // TH15
-    "1478369429380137222":
-    "1512350167209082981"
+    "1478369429380137222": "1512350167209082981",
+
+    // TH16
+    "SOURCE_TH16_ID": "DEST_TH16_ID",
+
+    // TH17
+    "SOURCE_TH17_ID": "DEST_TH17_ID",
+
+    // TH18
+    "SOURCE_TH18_ID": "DEST_TH18_ID"
 
 };
 
@@ -47,7 +62,7 @@ client.on("messageCreate", async (msg) => {
             "824653933347209227"
         ) return;
 
-        // FIND TARGET CHANNEL
+        // CHECK IF SOURCE CHANNEL EXISTS
         const targetChannelId =
             CHANNELS[msg.channel.id];
 
@@ -63,11 +78,38 @@ client.on("messageCreate", async (msg) => {
         if (!targetChannel)
             return;
 
-        // FORWARD MESSAGE
-        await msg.forward(targetChannel);
+        // GET EMBEDS
+        const embeds = msg.embeds;
+
+        // GET ATTACHMENTS
+        const files = [];
+
+        msg.attachments.forEach(att => {
+
+            files.push(att.url);
+
+        });
+
+        // SEND CLONED MESSAGE
+        await targetChannel.send({
+
+            content:
+                msg.content || null,
+
+            embeds:
+                embeds.length > 0
+                    ? embeds
+                    : [],
+
+            files:
+                files.length > 0
+                    ? files
+                    : []
+
+        });
 
         console.log(
-            `Forwarded from ${msg.channel.name}`
+            `Copied ClashKing message from ${msg.channel.name}`
         );
 
     } catch (err) {
